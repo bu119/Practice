7c1ebe47b3376a2f0bce6f894a71a1722bdb19a3
김부경 <sa01023@naver.com>
2022-12-24 18:18:08 +0900

    pjt

--- SSAFY 프로젝트/영화 추천 사이트 프로젝트 (관통_PJT)/final-pjt-back/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# from ..movies.models import Genre
-
-# Create your models here.
-class User(AbstractUser):
-    interested_genre1 = models.CharField(max_length=30, null=True)
-    interested_genre2 = models.CharField(max_length=30, null=True)
-    interested_genre3 = models.CharField(max_length=30, null=True)
